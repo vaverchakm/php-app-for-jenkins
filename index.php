c7409b6fc9c8c6f32a0ba669704a633869dca451
root <root@prdx_jenkins001.ziyotek.com>
2020-05-16 15:27:23 -0400

    Version 2.0

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1>Congratulations!</h1>
+        <h1><font color=red>Congratulations <br><font color=gold>DevOpsEngineer</h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
     </section>
@@ -44,6 +44,8 @@ else
             <li><a href="http://aws.amazon.com/php">PHP developer center</a></li>
             <li><a href="https://github.com/aws/aws-sdk-php">AWS SDK for PHP on GitHub</a></li>
         </ul>
+		<h2> Some Picture</h2>
+		<img src="chak.jpg">
     </section>
 
     <!--[if lt IE 9]><script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script><![endif]-->
